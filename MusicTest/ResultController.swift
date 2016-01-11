22d058ada83fff8e9e093b3a6dc8c8ea4e592563
Vladimir Gnatiuk <hash33r@gmail.com>
2016-01-11 17:15:11 +0200

    initial commit

---
+++ MusicTest/ResultController.swift
@@ -0,0 +1,9 @@
+//
+//  ResultController.swift
+//  MusicTest
+//
+//  Created by Vladimir Gnatiuk on 1/11/16.
+//  Copyright © 2016 Vladimir Gnatiuk. All rights reserved.
+//
+
+import Foundation
---
+++ MusicTest/SelectionGenreController.swift
@@ -0,0 +1,9 @@
+//
+//  SelectionGenreController.swift
+//  MusicTest
+//
+//  Created by Vladimir Gnatiuk on 1/11/16.
+//  Copyright © 2016 Vladimir Gnatiuk. All rights reserved.
+//
+
+import Foundation
